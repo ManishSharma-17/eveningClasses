27b05e014d7400121d838c877a2140ca3cd8821d
ManishSharma-17 <manishsincere17@gmail.com>
2022-04-26 12:49:39 +0530

    multiply the given value in the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,16 @@
  // 2. create function which increases the currnet value of p by 1
  // 3. add that function to button
 
- let adding = document.getElementById('addNum'); //0
+ let adding = document.getElementById('addNum'); //0 
+ console.log(adding.innerHTML);
 
  function addNum() {
+
+     // this one is for adding the given value
      adding.innerHTML = +adding.innerHTML + 1;
- }
+
+     // this one is for multiply the given value
+     adding.innerHTML = 1 + adding.innerHTML * 1;
+     console.log(adding.innerHTML);
+
+ };
