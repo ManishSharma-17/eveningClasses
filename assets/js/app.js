e23ca9ea1beec9e9e96c4206f3dbf651002527c0
ManishSharma-17 <manishsincere17@gmail.com>
2022-04-26 10:24:52 +0530

    activatign button

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@
  // 2. create function which increases the currnet value of p by 1
  // 3. add that function to button
 
- let addNum = document.getElementById('addNum'); //0
+ let adding = document.getElementById('addNum'); //0
 
  function addNum() {
-     addNum.innerHTML = +addNum.innerHTML + 1;
+     adding.innerHTML = +adding.innerHTML + 1;
  }
